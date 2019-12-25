0279ec19f5f9787ccbba51283c801d78bb1a6778
Giuliana Grecco <giu.grecco@gmail.com>
2019-12-25 14:34:58 -0700

    Adding RuleTest class and PlayCard Class on Game

--- UnoGame/src/main/java/Game.java
+++ UnoGame/src/main/java/Game.java
@@ -1,3 +1,12 @@
 public class Game {
+    //private Rules rule;
+    //Play Card
+    public void PlayCard(Player player, Card card){
+        if(player.getTurn()){
+            player.getCards().remove(card);
+        }
+        //rule.checkAction(,player,)
+
+    }
 
 }
---
+++ UnoGame/src/test/java/TestRules.java
@@ -0,0 +1,4 @@
+public class TestRules {
+    private static final Player player1;
+    private static final Player
+}
