92d7fe873a0e357e2e1bc0aade48383a78036c44
Suyeon Yang <yan17002@byui.edu>
2019-12-26 15:43:34 -0700

    robot class

--- UnoGame/src/main/java/Robot.java
+++ UnoGame/src/main/java/Robot.java
@@ -1,34 +1,46 @@
 public class Robot extends Player {
     //check the rulecolor
-    public void checkRuleColor(Game game, Robot robot) {
+    public int checkRuleColor(Game game, Robot robot) {
         Card card = game.getRuleCard();
         // counter. Count how many cards of that color you have.
-        int count = 0;
+        int colorCount = 0;
         for (int i = 0; i < robot.getCards().size(); i++) {
             if (robot.getCards().get(i).getColor().equals(card.getColor())) {
-                count++;
+                colorCount++;
             }
         }
+        return colorCount;
 
     }
     //check the value
     //check if he has a card with the value
-    public void checkRuleValue(Game game, Robot robot) {
+    public int checkRuleValue(Game game, Robot robot) {
         Card card = game.getRuleCard();
-        int count = 0;
+        int valueCount = 0;
         for (int i = 0; i < robot.getCards().size(); i++) {
             if (robot.getCards().get(i).getValue().equals(card.getValue())) {
                 for (int j = 0; j < robot.getCards().size(); j++) {
                     if (robot.getCards().get(i).getColor().equals(robot.getCards().get(j).getColor())) {
-                        count++;
+                        valueCount++;
                     }
                 }
             }
         }
+        return valueCount;
     }
 
-    //robot will choose the card with the more colors
+    //
 
+    //robot will choose the card with the more colors
+    public void chooseMoreColor(Game game, Robot robot) {
+        int colorCount = robot.checkRuleColor(game, robot);
+        int valueCount = robot.checkRuleColor(game, robot);
+        if (colorCount > valueCount) {
+
+        }
+
+
+    }
     //special card: if robot has it, choose that color of special card
 
     //if robot doesn't have any valid colors, and have special cards, then choose option randomly (including draw 1)
